c6489752d2bc1d547b9b9ea6ee7b4c33fcd8a8e3
hitesh <hiteshisgood@gmail.com>
2024-12-23 18:32:24 +0530

    Fix build configuration and App export

--- westx-frontend/src/index.js
+++ westx-frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';  // Make sure this import is correct
+import App from './App';  // Make sure this line matches exactly
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
